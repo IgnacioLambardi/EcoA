fc3674cd610468d9b7dac77959ab80583b8a7975
Económicas A <ecoa@fce.uba.ar>
2026-06-07 22:43:15 -0300

    Publicar datos: plantilla, partido vs Farmacia 3-1, tabla de posiciones

--- js/db-export.js
+++ js/db-export.js
@@ -1,3 +1,214 @@
 // Este archivo es generado automáticamente desde el Panel de Administración.
 // No lo edites a mano. Usá el botón "Publicar" del admin para actualizarlo.
-const EXPORTED_DATA = null;
+// Última publicación: 7/6/2026, 10:42:03
+const EXPORTED_DATA = {
+  "players": [
+    {
+      "id": "mq4d1iexu33e",
+      "name": "Iñigo Sagastusame",
+      "number": 1,
+      "position": "Arquero"
+    },
+    {
+      "id": "mq4d1my0bwbv",
+      "name": "Agustin \"Pitu\" Adorno",
+      "number": 4,
+      "position": "Defensor"
+    },
+    {
+      "id": "mq4d264oisbp",
+      "name": "Valentino Prego",
+      "number": 2,
+      "position": "Defensor"
+    },
+    {
+      "id": "mq4d2fx4a5md",
+      "name": "Gabriel Filoni",
+      "number": 6,
+      "position": "Defensor"
+    },
+    {
+      "id": "mq4d2lo8829y",
+      "name": "Iván Maidana",
+      "number": 3,
+      "position": "Defensor"
+    },
+    {
+      "id": "mq4d35dkhngo",
+      "name": "Lautaro Claro",
+      "number": 19,
+      "position": "Defensor"
+    },
+    {
+      "id": "mq4d56i7oc9d",
+      "name": "Agustín \"Corren\" Ayala",
+      "number": 5,
+      "position": "Mediocampista"
+    },
+    {
+      "id": "mq4d5ipkpl4w",
+      "name": "Fermín Chinchurreta",
+      "number": 8,
+      "position": "Mediocampista"
+    },
+    {
+      "id": "mq4d5x9r99kf",
+      "name": "Juan Pedro",
+      "number": 12,
+      "position": "Mediocampista"
+    },
+    {
+      "id": "mq4d67nkomki",
+      "name": "Juan Laborde",
+      "number": 7,
+      "position": "Delantero"
+    },
+    {
+      "id": "mq4d6kd3od6y",
+      "name": "Franco \"Flaco\" Angelone",
+      "number": 9,
+      "position": "Delantero"
+    },
+    {
+      "id": "mq4d6rts3fur",
+      "name": "Luli",
+      "number": 11,
+      "position": "Delantero"
+    },
+    {
+      "id": "mq4dr2kmn15r",
+      "name": "Francisco Montivero",
+      "number": 12,
+      "position": "Mediocampista"
+    },
+    {
+      "id": "mq4ds2kck5fg",
+      "name": "Bautista Jaspe",
+      "number": 15,
+      "position": "Mediocampista"
+    },
+    {
+      "id": "mq4dsddw4i6m",
+      "name": "Rodrigo Encinas",
+      "number": 10,
+      "position": "Mediocampista"
+    },
+    {
+      "id": "mq4dtgf0ri8h",
+      "name": "Mateo 🇫🇷",
+      "number": 18,
+      "position": "Delantero"
+    }
+  ],
+  "matches": [
+    {
+      "id": "mq4d13oia24j",
+      "opponent": "Farmacia",
+      "date": "2026-06-07",
+      "isHome": true,
+      "jornada": 1,
+      "played": true,
+      "goalsFor": 3,
+      "goalsAgainst": 1,
+      "convocados": [
+        "mq4d1iexu33e",
+        "mq4d1my0bwbv",
+        "mq4d264oisbp",
+        "mq4d2fx4a5md",
+        "mq4d2lo8829y",
+        "mq4d35dkhngo",
+        "mq4d56i7oc9d",
+        "mq4d5ipkpl4w",
+        "mq4d5x9r99kf",
+        "mq4d67nkomki",
+        "mq4d6kd3od6y",
+        "mq4d6rts3fur",
+        "mq4dr2kmn15r",
+        "mq4ds2kck5fg",
+        "mq4dsddw4i6m",
+        "mq4dtgf0ri8h"
+      ],
+      "scorers": [
+        {
+          "id": "mq4d1my0bwbv",
+          "goals": 1
+        },
+        {
+          "id": "mq4dtgf0ri8h",
+          "goals": 2
+        }
+      ],
+      "assists": [],
+      "captainId": "mq4d1iexu33e",
+      "yellows": [
+        "mq4d264oisbp"
+      ],
+      "reds": [],
+      "formation": "4-3-3",
+      "lineup": [
+        "mq4d1iexu33e",
+        "mq4d1my0bwbv",
+        "mq4d2fx4a5md",
+        "mq4d264oisbp",
+        "mq4d2lo8829y",
+        "mq4d5x9r99kf",
+        "mq4d56i7oc9d",
+        "mq4d5ipkpl4w",
+        "mq4d6rts3fur",
+        "mq4d6kd3od6y",
+        "mq4d67nkomki"
+      ],
+      "subs": [
+        {
+          "minute": 45,
+          "inId": "mq4dsddw4i6m",
+          "outId": "mq4d264oisbp"
+        },
+        {
+          "minute": 45,
+          "inId": "mq4dtgf0ri8h",
+          "outId": "mq4d6kd3od6y"
+        },
+        {
+          "minute": 45,
+          "inId": "mq4d5ipkpl4w",
+          "outId": "mq4ds2kck5fg"
+        },
+        {
+          "minute": 55,
+          "inId": "mq4dr2kmn15r",
+          "outId": "mq4d6rts3fur"
+        },
+        {
+          "minute": 65,
+          "inId": "mq4d35dkhngo",
+          "outId": "mq4d1my0bwbv"
+        }
+      ]
+    }
+  ],
+  "zoneMatches": [
+    {
+      "id": "mq4f6fj2i49s",
+      "zona": "zona2",
+      "jornada": 1,
+      "homeTeam": "Económicas A",
+      "awayTeam": "Farmacia",
+      "goalsHome": 3,
+      "goalsAway": 1,
+      "played": true
+    },
+    {
+      "id": "mq4fcxh5miqg",
+      "zona": "zona2",
+      "jornada": null,
+      "homeTeam": "Filo B",
+      "awayTeam": "FUBA",
+      "status": "no_show",
+      "played": false,
+      "goalsHome": 0,
+      "goalsAway": 0,
+      "noShowTeam": "home"
+    }
+  ]
+};
